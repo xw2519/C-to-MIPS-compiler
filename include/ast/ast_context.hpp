6146284018fe508345a600969f2255b769f588df
ebbys <ebbysamson@gmail.com>
2021-03-05 10:58:55 +0000

    commented out declarations

--- include/ast/ast_context.hpp
+++ include/ast/ast_context.hpp
@@ -126,27 +126,27 @@ struct Context
 
     /* ------------------------------------				  Functions for code generation				  ------------------------------------ */
 
-    std::string get_float_label(double value)                                   // get label of  ".word <float value> directive"
-    std::string get_string_label(std::String value)                             // get label of  ".ascii <string literal/000> directive"
-    std::string make_label()                                                    // generate unique label and return as string
+    //std::string get_float_label(double value)                                   // get label of  ".word <float value> directive"
+    //std::string get_string_label(std::String value)                             // get label of  ".ascii <string literal/000> directive"
+    //std::string make_label()                                                    // generate unique label and return as string
 
-    std::string next_reg(std::string someReg)                                   // return name of next register
-    std::string alloc_reg(ExpressionEnum type, int amount=1)                    // return name of free register, mark it and following amount as occupied
-    void dealloc_reg(std::string someReg, int amount=1)                         // free register and following amount of registers
+    //std::string next_reg(std::string someReg)                                   // return name of next register
+    //std::string alloc_reg(ExpressionEnum type, int amount=1)                    // return name of free register, mark it and following amount as occupied
+    //void dealloc_reg(std::string someReg, int amount=1)                         // free register and following amount of registers
 
-    bool check_global(std::string identifier)                                   // return true if identifier is global
+    //bool check_global(std::string identifier)                                   // return true if identifier is global
 
-    ExpressionEnum get_type(std::string identifier)                             // return type of identifier
-    ExpressionEnum get_type_pointed(std::string identifier)                     // return type pointed to by identifier
-    ExpressionEnum get_type_member(std::string identifier, std::string member)  // return type of member of struct
+    //ExpressionEnum get_type(std::string identifier)                             // return type of identifier
+    //ExpressionEnum get_type_pointed(std::string identifier)                     // return type pointed to by identifier
+    //ExpressionEnum get_type_member(std::string identifier, std::string member)  // return type of member of struct
 
-    std::string id_to_addr(std::string identifier)                              // return address, relative to $fp, of identifier
-    std::string member_to_addr(std::string identifier, std::string member)      // return address of member, relative to beginning of struct
+    //std::string id_to_addr(std::string identifier)                              // return address, relative to $fp, of identifier
+    //std::string member_to_addr(std::string identifier, std::string member)      // return address of member, relative to beginning of struct
 
-    int size_of_pointed(std::string identifier)                                 // return size of value pointed to by identifier, in words
-    int size_of_member(std::string identifier, std::string member);             // return size of member, in words
-    int size_of(std::string identifier)                                         // return size of identifier in words
-    int size_of(Declaration* declr)                                             // return size of declaration
+    //int size_of_pointed(std::string identifier)                                 // return size of value pointed to by identifier, in words
+    //int size_of_member(std::string identifier, std::string member);             // return size of member, in words
+    //int size_of(std::string identifier)                                         // return size of identifier in words
+    //int size_of(Declaration* declr)                                             // return size of declaration
 
 
 };
